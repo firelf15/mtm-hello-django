aef0ce5fdabb20e42b403a593278da7f39a643c1
Monika Spielman <monika.spielman@metaltoad.com>
2015-05-07 16:38:07 -0700

    Back to part 1 of tutorial

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#Monika added this next line    
+    'hello_scooby',
 )
 
 MIDDLEWARE_CLASSES = (
